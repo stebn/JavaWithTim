e2f51233e27c75bbbb5f26e19f34401626bb2b01
Tanya <t.stebneva@mail.ru>
2017-10-04 12:51:57 -0400

    Initial checkin

---
+++ JavaWithTim/src/assignments/FirstAssignment.java
@@ -0,0 +1,34 @@
+package assignments;
+
+public class FirstAssignment {
+
+	public static void main(String[] args) {
+		
+		int scorePosition = calculateHighScorePosition(1000);
+		displayHighscorePosition("Tim", scorePosition );
+		
+		scorePosition = calculateHighScorePosition(500);
+		displayHighscorePosition("Sam", scorePosition );
+		
+		scorePosition = calculateHighScorePosition(100);
+		displayHighscorePosition("Pete", scorePosition );
+		
+		scorePosition = calculateHighScorePosition(70);
+		displayHighscorePosition("Tom", scorePosition );
+
+	}
+
+	public static void displayHighscorePosition(String PlayerName, int position) {
+		
+		System.out.println(PlayerName + " managed to get on the " + position + " position.");
+
+	}
+
+	public static int calculateHighScorePosition(int score) {
+		if (score >= 1000) return 1;
+		else if (score <1000 && score >=500) return 2;
+		else if (score <500 && score >=100) return 3;
+		return 4;
+	}
+
+}
---
+++ JavaWithTim/src/assignments/SecondAssignment.java
@@ -0,0 +1,34 @@
+package assignments;
+
+public class SecondAssignment {
+
+	public static void main(String[] args) {
+		calcFeetAndInchesToCentimeters(500);
+	}
+
+	public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+		double cent;
+		if (feet >= 0 || inches >= 0 || inches <= 12) {
+			//cent = (feet * 30.48) + (inches * 2.54);
+			cent = ((feet * 12) + inches) *2.54;
+			System.out.println(cent + " cm");
+			return cent;
+		} else {
+			System.out.println("Invalid");
+			return -1;
+		}
+	}
+
+	public static double calcFeetAndInchesToCentimeters(double inches) {
+		if (inches < 0) {
+			System.out.println("Invalid");
+			return -1;
+		}
+
+		double feet = (int)inches / 12;
+		double remainingInches = (int)inches % 12;
+		System.out.println(feet + " ft " + remainingInches + " inches");
+		return calcFeetAndInchesToCentimeters(feet, remainingInches);
+
+	}
+}
